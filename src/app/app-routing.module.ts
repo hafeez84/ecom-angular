46168d1360caa2cf807429b50679fd36a1d29044
hafeez84 <h.qasemy@live.com>
2019-04-07 17:29:17 +0300

    added set product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings/settings.component';
 import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { SetproductComponent } from './setproduct/setproduct.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/aboutus', pathMatch: 'full'},
   {path: 'aboutus', component: AboutusComponent},
   {path: 'settings', component: SettingsComponent},
+  {path: 'setproduct', component: SetproductComponent},
   {path: '**', redirectTo: '/aboutus', pathMatch:'full'}
 
 ];
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,120 @@ import { Injectable } from '@angular/core';
 export class BackendService {
 
   constructor() { }
+
+  getCardTotal(){
+    let fake = "10";
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+  getUserStatus(){
+    let fake = true;
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+
+  getProducts(callType){
+    let fake = [{
+      'category': "test",
+      'scategory': "test",
+      'name': "test",
+      'price': "123",
+      '_id': "545"
+    }];
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+  getFilterProducts(callType, filter){
+    let fake = [{
+      'category': "test",
+      'scategory': "test",
+      'name': "test",
+      'price': "123",
+      '_id': "545"
+    }];
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+  
+  setProducts(filter, formdata){
+    let fake = true;
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+  updateProducts(formdata){
+    let fake = true;
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+  getOneProductDoc(docId){
+    let fake = {
+      'category': "test",
+      'scategory': "test",
+      'name': "test",
+      'price': "123",
+      '_id': "545"
+    };
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+
+
+  delOneProductDoc(docId){
+    let fake = true;
+    return Observable.create(
+      observable => {
+        setTimeout(() => {
+          observable.next(fake)
+        }, 2000);
+      }
+    )
+  }
+
+
+
+
+
+
+  
 }
